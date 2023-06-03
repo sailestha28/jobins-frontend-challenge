f2ea9dafe05fde0bb2041402648a2ba3ce6d7abe
sailestha <saileshta2018@gmail.com>
2023-06-03 21:39:18 +0545

    feat: page file

---
+++ src/views/dashboard/DashboardView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import SaleAndCostsWidget from "../../components/dashboard/widgets/SaleAndCostsWidget.vue";
+</script>
+
+<template>
+  <section>
+
+    <SaleAndCostsWidget />
+  </section>
+</template>
