b0916128706db39295f7cb2fc5c6eaf5980e54c8
sailestha <saileshta2018@gmail.com>
2023-06-07 19:36:05 +0545

    create table view

---
+++ src/components/dashboard/TableView.vue
@@ -0,0 +1,393 @@
+<template>
+  <div class="mt-3">
+    <div class="mb-5 flex flex-wrap sm:flex-nowrap gap-x-4">
+      <div class="mb-5 sm:mb-0 w-full sm:w-3/5 flex gap-x-4">
+        <div class="min-w-[150px]">
+          <v-select
+            v-model="filter.status"
+            :items="statusData"
+            label=""
+            placeholder="Status: All"
+            density="compact"
+            class="normal-v-select"
+          >
+            <template v-slot:item="{ item, props }">
+              <v-list-item v-bind="props">
+                <template v-slot:title>
+                  {{ item.raw }}
+                </template>
+              </v-list-item>
+            </template>
+          </v-select>
+        </div>
+        <div class="w-[200px]">
+          <v-text-field
+            v-model="filter.search"
+            density="compact"
+            variant="solo"
+            class="v-text-filed"
+            append-inner-icon="mdi-magnify"
+            placeholder="Search"
+            single-line
+            hide-details
+            @click:append-inner="() => onSearch"
+          ></v-text-field>
+        </div>
+      </div>
+      <div class="flex sm:justify-end w-full">
+        <div class="w-full max-w-[350px] sm:w-[250px]">
+          <VueDatePicker
+            class="vue_date_picker"
+            placeholder="Filter by date range"
+            v-model="filter.dateRange"
+            format="dd MMMM, yyyy - dd MMMM, yyyy"
+            range
+          />
+        </div>
+      </div>
+    </div>
+    <v-data-table
+      :headers="headers"
+      :items="tableData"
+      class="text-[#23272E] text-[15px] rounded-10 rounded-bl-[0px] rounded-br-[0px] overflow-hidden bg-transparent"
+      item-key="name"
+      :loading="loading"
+      loading-text="Loading... Please wait"
+      :items-per-page="itemsPerPage"
+      :mobile-breakpoint="950"
+      v-model:page="page"
+      :search="filter.search"
+      :footer-props="{
+        'items-per-page-text': 'products per page',
+      }"
+    >
+      <template v-slot:column.id="{ column }">
+        <span class="text-[#8B909A] text-base">
+          {{ column.title.toUpperCase() }}
+        </span>
+      </template>
+      <template v-slot:column.customer="{ column }">
+        <span class="text-[#8B909A] text-sm"> {{ column.title.toUpperCase() }}</span>
+      </template>
+      <template v-slot:column.date="{ column }">
+        <span class="text-[#8B909A] text-sm">
+          {{ column.title.toUpperCase() }}
+        </span>
+      </template>
+      <template v-slot:column.total="{ column }">
+        <span class="text-[#8B909A] text-sm">
+          {{ column.title.toUpperCase() }}
+        </span>
+      </template>
+      <template v-slot:column.method="{ column }">
+        <span class="text-[#8B909A] text-sm">
+          {{ column.title.toUpperCase() }}
+        </span>
+      </template>
+
+      <template v-slot:column.action="{ column }">
+        <span class="text-[#8B909A] text-sm">
+          {{ column.title.toUpperCase() }}
+        </span>
+      </template>
+      <template v-slot:item.state="{ item }">
+        <span
+          :class="[
+            item.selectable.state.toLowerCase() === 'pending' ? 'text-[#FFC600]' : null,
+            item.selectable.state.toLowerCase() === 'cancelled' ? 'text-[#EA5455]' : null,
+            item.selectable.state.toLowerCase() === 'completed' ? 'text-[#28C76F]' : null,
+          ]"
+        >
+          {{ item.selectable.state }}
+        </span>
+      </template>
+      <template v-slot:item.action="{ item }">
+        <div class="flex gap-x-1">
+          <button class="text-[#0F60FF]" @click.prevent="() => onView(item.columns)">
+            View
+          </button>
+          <button class="text-[#0F60FF]" @click.prevent="() => onDetail(item.columns)">
+            Detail
+          </button>
+        </div>
+      </template>
+
+      <template v-slot:bottom>
+        <div class="flex justify-between bg-white border-lb-[20px] vDataTable_footer">
+          <div class="w-[300px] flex items-center gap-x-2">
+            <p class="text-[#8B909A] text-xs">Showing</p>
+            <v-select
+              v-model="itemsPerPage"
+              :items="perPageOptions"
+              label=""
+              placeholder="Status: All"
+              density="compact"
+              class="normal-v-select"
+            ></v-select>
+            <span class="text-[#8B909A] text-xs"> of {{ tableData.length }} </span>
+          </div>
+
+          <v-pagination
+            class="text-xs"
+            v-model="page"
+            :length="pageCount"
+            :total-visible="5"
+          ></v-pagination>
+        </div>
+      </template>
+    </v-data-table>
+  </div>
+</template>
+
+<script>
+import { VDataTable } from "vuetify/labs/VDataTable";
+import VueDatePicker from "@vuepic/vue-datepicker";
+import "@vuepic/vue-datepicker/dist/main.css";
+export default {
+  props: { column: Array, filterValue: String },
+  components: {
+    VDataTable,
+    VueDatePicker,
+  },
+  watch: {
+    column: function (newValue, oldValue) {
+      this.headers = newValue;
+    },
+    filterValue: function (newValue, oldValue) {
+      this.getDataOnFilter();
+    },
+    itemsPerPage: function (newValue, oldValue) {
+      this.getDataOnFilter();
+    },
+  },
+  data() {
+    return {
+      filter: { status: null, search: null, dateRange: null },
+      page: 1,
+      itemsPerPage: 10,
+      perPageOptions: [5, 10, 15, 20, 25, 50],
+      loading: false,
+      headers: this.column || [],
+      statusData: ["All", "Active", "Pending", "Cancelled", "Completed"],
+      tableData: [],
+      pageCount: null,
+    };
+  },
+  mounted() {
+    this.getDataOnFilter();
+  },
+  computed: {
+    // pageCount() {
+    //   return Math.ceil(this.tableData.length / this.itemsPerPage);
+    // },
+  },
+  methods: {
+    onSearch() {
+      console.log("click search");
+    },
+    onDetail(data) {
+      console.log(data);
+    },
+    onView(data) {
+      console.log(data);
+    },
+    getDataOnFilter() {
+      const tableArray = [
+        {
+          id: "#5089",
+          customer: "Joseph Doe",
+          date: "6 April, 2023",
+          total: "$2,564",
+          state: "Pending",
+          method: "CC",
+        },
+        {
+          id: "#5090",
+          customer: "Joseph Xin",
+          date: "6 April, 2023",
+          total: "$2,564",
+          state: "Cancelled",
+          method: "CC",
+        },
+        {
+          id: "#5089",
+          customer: "Joseph Doe",
+          date: "6 April, 2023",
+          total: "$2,564",
+          state: "Pending",
+          method: "CC",
+        },
+        {
+          id: "#5090",
+          customer: "Joseph Xin",
+          date: "6 April, 2023",
+          total: "$2,564",
+          state: "Cancelled",
+          method: "CC",
+        },
+        {
+          id: "#5089",
+          customer: "Joseph Doe",
+          date: "6 April, 2023",
+          total: "$2,564",
+          state: "Pending",
+          method: "CC",
+        },
+        {
+          id: "#5090",
+          customer: "Joseph Xin",
+          date: "6 April, 2023",
+          total: "$2,564",
+          state: "Cancelled",
+          method: "CC",
+        },
+        {
+          id: "#5089",
+          customer: "Joseph Doe",
+          date: "6 April, 2023",
+          total: "$2,564",
+          state: "Pending",
+          method: "CC",
+        },
+        {
+          id: "#5090",
+          customer: "Joseph Xin",
+          date: "6 April, 2023",
+          total: "$2,564",
+          state: "Cancelled",
+          method: "CC",
+        },
+        {
+          id: "#5089",
+          customer: "Joseph Doe",
+          date: "6 April, 2023",
+          total: "$2,564",
+          state: "Pending",
+          method: "CC",
+        },
+        {
+          id: "#5090",
+          customer: "Joseph Xin",
+          date: "6 April, 2023",
+          total: "$2,564",
+          state: "Cancelled",
+          method: "CC",
+        },
+        {
+          id: "#5091",
+          customer: "Jhon Doe",
+          date: "6 April, 2023",
+          total: "$2,564",
+          state: "Completed",
+          method: "CC",
+        },
+        {
+          id: "#5092",
+          customer: "Joseph Wheeler",
+          date: "6 April, 2023",
+          total: "$2,564",
+          state: "Completed",
+          method: "CC",
+        },
+
+        {
+          id: "#5089",
+          customer: "Joseph Wheeler",
+          date: "6 April, 2023",
+          total: "$2,564",
+          state: "Completed",
+          method: "CC",
+        },
+        {
+          id: "#5089",
+          customer: "Joseph Wheeler",
+          date: "6 April, 2023",
+          total: "$2,564",
+          state: "Cancelled",
+          method: "CC",
+        },
+        {
+          id: "#5089",
+          customer: "Joseph Wheeler",
+          date: "6 April, 2023",
+          total: "$2,564",
+          state: "Pending",
+          method: "CC",
+        },
+        {
+          id: "#5089",
+          customer: "Joseph Wheeler",
+          date: "6 April, 2023",
+          total: "$2,564",
+          state: "Pending",
+          method: "CC",
+        },
+        {
+          id: "#5089",
+          customer: "Joseph Wheeler",
+          date: "6 April, 2023",
+          total: "$2,564",
+          state: "Pending",
+          method: "CC",
+        },
+        {
+          id: "#5089",
+          customer: "Joseph Wheeler",
+          date: "6 April, 2023",
+          total: "$2,564",
+          state: "Pending",
+          method: "CC",
+        },
+        {
+          id: "#5089",
+          customer: "Joseph Wheeler",
+          date: "6 April, 2023",
+          total: "$2,564",
+          state: "Completed",
+          method: "CC",
+        },
+        {
+          id: "#5089",
+          customer: "Joseph Wheeler",
+          date: "6 April, 2023",
+          total: "$2,564",
+          state: "Pending",
+          method: "CC",
+        },
+        {
+          id: "#5089",
+          customer: "Joseph Wheeler",
+          date: "6 April, 2023",
+          total: "$2,564",
+          state: "Pending",
+          method: "CC",
+        },
+      ];
+      if (this.filterValue === "all") {
+        this.loading = true;
+        setTimeout(() => {
+          this.tableData = tableArray;
+          this.loading = false;
+          this.pageCount = Math.ceil(this.tableData.length / this.itemsPerPage);
+        }, 1000);
+      } else if (this.filterValue === "completed") {
+        this.loading = true;
+        setTimeout(() => {
+          const completedData = tableArray.filter((item) => item.state === "Completed");
+          this.tableData = completedData;
+          this.loading = false;
+          this.pageCount = Math.ceil(this.tableData.length / this.itemsPerPage);
+        }, 1000);
+      } else {
+        this.loading = true;
+        setTimeout(() => {
+          const calcelledData = tableArray.filter((item) => item.state === "Cancelled");
+          this.tableData = calcelledData;
+          this.loading = false;
+          this.pageCount = Math.ceil(this.tableData.length / this.itemsPerPage);
+        }, 1000);
+      }
+    },
+  },
+};
+</script>
