1ef20258801af6b0a4fcf2c445ff706aaf1f0a82
sailestha <saileshta2018@gmail.com>
2023-06-08 18:57:33 +0545

    feat: finalized the project

--- src/components/dashboard/TabStatus.vue
+++ src/components/dashboard/TabStatus.vue
@@ -3,18 +3,17 @@
     <v-window v-model="tab" :touch="false" class="overflow-visible">
       <v-window-item value="all">
         <div class="mt-3">
-        <TableView :column="headers" :filterValue="tab" />
+          <TableView :column="headers" :filterValue="tab" />
         </div>
       </v-window-item>
       <v-window-item value="completed" :filterValue="tab">
         <div class="mt-3">
-        <TableView :column="headers" :filterValue="tab" />
-            </div>
+          <TableView :column="headers" :filterValue="tab" />
+        </div>
       </v-window-item>
       <v-window-item value="cancelled">
         <div class="mt-3">
-
-        <TableView :column="headers" :filterValue="tab" />
+          <TableView :column="headers" :filterValue="tab" />
         </div>
       </v-window-item>
     </v-window>
@@ -50,12 +49,12 @@ export default {
           key: "id",
           width: "85px",
         },
-        { title: "Customer", align: "start", key: "customer", width: "200px" },
+        { title: "Customer", align: "start", key: "customer" },
         { title: "Date", align: "start", key: "date", width: "135px" },
         { title: "Total", align: "start", key: "total", width: "115px" },
         { title: "Method", align: "start", key: "method", width: "100px" },
         { title: "Status", align: "start", key: "state", width: "140px" },
-        { title: "Action", align: "start", key: "action" },
+        { title: "Action", align: "start", key: "action", width: "140px" },
       ],
     };
   },
