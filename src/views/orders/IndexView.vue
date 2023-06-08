74a870cfb074a4e880aec6e69cf961f192655e22
sailestha <saileshta2018@gmail.com>
2023-06-08 08:39:46 +0545

    table and forms added

--- src/views/orders/IndexView.vue
+++ src/views/orders/IndexView.vue
@@ -1,6 +1,37 @@
 <template>
-  <div >
-    <h1>Order Management page</h1>
+  <div class="mt-1">
+    <TableView :column="headers" />
   </div>
 </template>
 
+<script>
+import TableView from "../../components/dashboard/TableView.vue";
+import { Data } from "@/data/data.js";
+
+export default {
+  name: "OrderView",
+  components: { TableView },
+  data() {
+    return {
+      onData: Data,
+      headers: [
+        {
+          title: "ID",
+          align: "start",
+          sortable: false,
+          key: "id",
+          width: "85px",
+        },
+        { title: "Customer", align: "start", key: "customer", width: "200px" },
+        { title: "Date", align: "start", key: "date", width: "135px" },
+        { title: "Total", align: "start", key: "total", width: "115px" },
+        { title: "Method", align: "start", key: "method", width: "100px" },
+        { title: "Status", align: "start", key: "state", width: "140px" },
+        { title: "Action", align: "start", key: "action" },
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>
